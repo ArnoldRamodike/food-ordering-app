18774a5f3625737908ce42dcca80eab5279c394a
ArnoldRamodike <riccoarnold23@gmail.com>
2024-03-06 23:00:32 +0200

    userinfo schema and User schema updates

---
+++ src/app/models/UserInfo.js
@@ -0,0 +1,29 @@
+const { Schema, models, model } = require("mongoose");
+
+const UserInfoSchema = new Schema ({
+    email: {
+        type: String, 
+        required: true, 
+    },
+    phone:{
+        type: String
+    },
+    streetAddress: {
+        type: String
+    },
+    city: {
+        type: String
+    },
+    postalCode: {
+        type: String
+    },
+    country: {
+        type: String
+    },
+    admin: {
+        type: Boolean,
+        default: true
+    },
+}, {timeStamp: true});
+
+export const UserInfo = models?.UserInfo  || model("UserInfo", UserInfoSchema);
