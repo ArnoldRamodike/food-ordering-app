ad9c41b9ae9f7db79838db6d08cd09537cec6a9e
ArnoldRamodike <riccoarnold23@gmail.com>
2024-03-08 15:36:21 +0200

    category Model

---
+++ src/app/models/Category.js
@@ -0,0 +1,12 @@
+const { Schema, models, model } = require("mongoose");
+
+const CategorySchema = new Schema ({
+    name: {
+        type: String, 
+        required: true, 
+    },
+
+
+}, {timeStamp: true});
+
+export const Category = models?.Category  || model("Category", CategorySchema);
