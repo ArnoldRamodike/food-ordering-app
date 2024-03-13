98691c5373ac30ca2f58d0aacfbaf707405285e4
ArnoldRamodike <riccoarnold23@gmail.com>
2024-03-13 20:15:17 +0200

    Menu Items Page Crude

--- src/app/menu-items/page.js
+++ src/app/menu-items/page.js
@@ -1,16 +1,27 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import UserTabs from "@/components/layout/UserTabs";
 import {useProfile} from "@/components/layout/UseProfile";
 import Link from 'next/link';
+import Image from 'next/image';
 import EditableImage from '@/components/layout/EditableImage';
 import toast from "react-hot-toast";
 import Right from '@/components/icons/Right';
+import MenuItem from '@/components/menu/MenuItem';
 
 const MenuItemsPage = () => {
 
     const {loading, data} = useProfile();
+    const [MenuItems, setMenuItems] = useState([]);
+
+    useEffect(() => {
+        fetch('/api/menu-items').then(res => {
+            res.json().then(MenuItem => {
+                setMenuItems(MenuItem);
+            })
+        } )
+    }, []);
 
     if (loading) {
         return 'Loading User Info ...';
@@ -27,6 +38,24 @@ const MenuItemsPage = () => {
             <span>Create new</span>
             <Right/> 
         </Link>
+       </div>
+       <div>
+        <h2 className='text-sm text-gray-500 mt-8'>Edid Menu Items</h2>
+        <div className='grid grid-cols-3 gap-2'>
+          {MenuItems?.length > 0 && MenuItems.map(item => (
+            <Link href={'menu-items/edit/'+item._id} key={item._id} 
+                className='bg-gray-200 rounded-lg p-4 button '>
+                <div className='relative'>
+                <Image className='rounded-md' src={item.image} alt={'Item Image'} width={100} height={100} />
+                </div>
+                <div className='text-center'>
+                    {item.name}
+                </div>
+               
+            </Link>
+           ))}
+        </div>
+        
        </div>
   
     </section>
