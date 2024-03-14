2f892c24992fb4491d1ec8582ded12635f37074a
ArnoldRamodike <riccoarnold23@gmail.com>
2024-03-14 14:12:38 +0200

    Toggle Prices

--- src/components/layout/MenuItemsProps.js
+++ src/components/layout/MenuItemsProps.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Trash from '@/components/icons/Trash';
 import Plus from '@/components/icons/Plus';
+import Up from '@/components/icons/ChevronUp';
+import Down from '@/components/icons/ChevronDown';
 
 export default function MenuItemsPriceProps ({name,addLabel, props, setProps})  {
+
+    const [isOpen, setIsOpen] = useState(false);
 
     function addProp() {
         setProps(oldProps => {
@@ -26,7 +30,21 @@ export default function MenuItemsPriceProps ({name,addLabel, props, setProps})  
   
   return (
     <div className='bg-gray-200 p-2 rounded-md mb-2'>
-        <label>{name}</label>
+
+            <button type='button' className='inline-flex p-1 justify-start' onClick={() => setIsOpen(prev => !isOpen)}>              
+
+                <span className='text-gray-700'>{name}</span> 
+                <span className='text-gray-700'>({props?.length})</span> 
+                {isOpen && ( 
+                    <Up/>
+                )}
+                 {!isOpen && ( 
+                    <Down/> 
+                )}
+            </button>
+            <div className={isOpen ? 'block' : 'hidden'}>
+            
+     
         {props?.length > 0 && props.map((size, index) => (
           <div className='flex items-end gap-2' key={index}>
             <div>
@@ -46,6 +64,7 @@ export default function MenuItemsPriceProps ({name,addLabel, props, setProps})  
     <button type='button' onClick={addProp} className='bg-white items-center'> 
         <Plus className='w-4 h-4'/> {addLabel}
     </button>
+    </div>    
   </div>
   )
 }
