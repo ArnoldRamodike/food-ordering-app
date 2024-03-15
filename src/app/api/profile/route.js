9f316a3992026fd7fa7dd9b6462e9672ce84fbff
ArnoldRamodike <riccoarnold23@gmail.com>
2024-03-15 22:08:40 +0200

    User/Profile Route update

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -8,28 +8,49 @@ export async function PUT(req){
     mongoose.connect(process.env.MONGO_URl);
 
     const data = await req.json();
-    const {name, image, ...otherUserInfo} = data
-    const session = await getServerSession(authOptions);
-    const email = session.user.email;
+    const {id, name, image, ...otherUserInfo} = data;
+
+    let filter = {};
+    if (_id) {
+        filter = {_id}
+    }else{
+        const session = await getServerSession(authOptions);
+        const email = session.user.email; 
+        filter= {email};
+    }
 
         //update all fields
-        await User.updateOne({email}, {name: data.name, image: data.image});
-        await UserInfo.findOneAndUpdate({email}, otherUserInfo, {upsert: true});
+        const user = await User.findOne(filter);
+        await User.updateOne(filter, {name, image});
+        await UserInfo.findOneAndUpdate({email: user.email}, otherUserInfo, {upsert: true});
     return Response.json(true);
 
 }
 
 export async function GET(){
     mongoose.connect(process.env.MONGO_URl);
-    const session = await getServerSession(authOptions);
-    const email = session?.user?.email;
-    if (!email) {
-        return Response.json({});
-    }
 
-    const user =  await User.findOne({email}).lean();
-    const userInfo =  await UserInfo.findOne({email}).lean();
-    return Response.json(
-      {...user, ...userInfo} 
-    );
+    const url = new URL(req.url);
+    url.searchParams.get('_id');
+
+    let filterUser= {};
+    if (_id) {
+        filterUser = {_id};
+
+
+    }else{
+         const session = await getServerSession(authOptions);
+         const email = session?.user?.email;
+         if (!email) {
+            return Response.json({});
+         }
+         filterUser = {email};
+    } 
+
+    const user =  await User.findOne({_id}).lean();
+    const userInfo =  await UserInfo.findOne({email: user.email}).lean();
+
+    return Response.json({...user, ...userInfo} );
 }
+
+ 
