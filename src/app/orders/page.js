ba8631e1138d8c35ed3f21f5d7fcc0be55eae711
ArnoldRamodike <riccoarnold23@gmail.com>
2024-03-15 21:25:31 +0200

    Minor updates

--- src/app/orders/page.js
+++ src/app/orders/page.js
@@ -4,11 +4,11 @@ import React, { useState } from 'react'
 import UserTabs from "@/components/layout/UserTabs";
 import {useProfile} from "@/components/layout/UseProfile";
 
-const Orders = () => {
+const OrdersPage = () => {
 
     const {loading, data} = useProfile();
     const [redirectToItem, setRedirectToItem] = useState(false);
-
+    
 
     if (redirectToItem) {
         return redirect('/users');
@@ -23,11 +23,11 @@ const Orders = () => {
     }
 
   return (
-    <section className='mt-8'>
+    <section className='max-w-2xl mx-auto mt-8'>
         <UserTabs isAdmin={true} />
         orders
     </section>
   )
 }
 
-export default Orders
+export default OrdersPage
