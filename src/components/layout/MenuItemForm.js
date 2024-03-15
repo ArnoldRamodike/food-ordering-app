e1cc355867573e161943424b6c9d45d15fe57067
ArnoldRamodike <riccoarnold23@gmail.com>
2024-03-15 09:43:58 +0200

    Select Category on Menu-Items

--- src/components/layout/MenuItemForm.js
+++ src/components/layout/MenuItemForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import EditableImage from '@/components/layout/EditableImage';
 import MenuItemPriceProps from '@/components/layout/MenuItemsProps'
 
@@ -8,12 +8,23 @@ export default function MenuItemForm ({onSubmit, menuItem}) {
     const [name, setName] = useState(menuItem?.name || '');
     const [description, setDescription] = useState(menuItem?.description || '');
     const [basePrice, setBasePrice] = useState(menuItem?.basePrice || '');
-    const [sizes, setSizes] = useState(menuItem?.sizes||[]);
+    const [sizes, setSizes] = useState(menuItem?.sizes || []);
+    const [category, setCategory] = useState(menuItem.category || '')
     const [extraIngridientPrices, setExtraIngridientPrices] = useState(menuItem?.extraIngridientPrices || []);
+    const [categories, setCategories] = useState([]);
+    
+    useEffect(() => {
+      fetch('/api/categories').then(res => {
+        res.json().then(categories => {
+        setCategories(categories)
+        });
+      });
+    }, [])
+    
 
     return (
     <>
-        <form onSubmit={ ev => onSubmit(ev, {image, name, description, basePrice, sizes, extraIngridientPrices})} 
+        <form onSubmit={ ev => onSubmit(ev, {image, name, description, basePrice, sizes, extraIngridientPrices, category})} 
             className="mt-8 max-w-md mx-auto">
          <div className='grid items-start gap-4' style={{gridTemplateColumns: '.3fr .7fr'}}>
             <div>
@@ -25,6 +36,12 @@ export default function MenuItemForm ({onSubmit, menuItem}) {
              <input type='text' value={name} onChange={ev => setName(ev.target.value)}/> 
              <label>Description</label> 
              <input type='text'  value={description} onChange={ev => setDescription(ev.target.value)}/> 
+             <label>Category</label> 
+             <select value={category} onChange={ev => setCategory(ev.target.value)}>
+              {categories?.length > 0 && categories.map(c => (
+                <option value={c._id}>{c.name}</option>
+              ))}
+              </select>
              <label>Base price</label> 
              <input type='text'  value={basePrice} onChange={ev => setBasePrice(ev.target.value)}/> 
 
