289c8b2ed76d90704c91ded0229ed5db8f13a422
ArnoldRamodike <riccoarnold23@gmail.com>
2024-03-16 13:32:42 +0200

    Cart Context Provider

--- src/components/appContext.js
+++ src/components/appContext.js
@@ -1,12 +1,44 @@
 'use client'
 
-import React from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import {SessionProvider} from 'next-auth/react'
 
+export const cartContext = createContext({});
+
 const AppProvider = ({children}) => {
+
+  const [cartProducts, setCartProducts] = useState([]);
+  const ls = typeof window !== 'undefined' ? window.localStorage : null;
+
+  useEffect(() => {
+    if (ls && ls.getItem('cart')) {
+      setCartProducts(JSON.parse(ls.getItem('cart')));
+    }  
+  }, []);
+
+  function saveCartPoduactsToLocalStorage(cartProducts) {
+    if (ls) {
+      ls.setItem('cart', JSON.stringify(cartProducts));
+    }
+  }
+
+
+
+  function addToCart(product, size = null, extras=[]) {
+    setCartProducts( previousProducts => {
+      const cartProduct = {...product, size, extras};
+      const newProducts = [...previousProducts, cartProduct];
+      saveCartPoduactsToLocalStorage(newProducts);
+      return newProducts;
+    });
+  }
+
   return (
    <SessionProvider>
-    {children}
+    <cartContext.Provider value={{cartProducts, setCartProducts, addToCart,}}>
+          {children}
+    </cartContext.Provider>
+
    </SessionProvider>
   )
 }
