bddb66ec222863d9421a38fd112c30a5bb780c60
ArnoldRamodike <riccoarnold23@gmail.com>
2024-03-16 11:54:50 +0200

    Users Page

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,17 +1,22 @@
 'use client'
 
-import React, { useState }  from 'react'
+import React, { useEffect, useState }  from 'react'
+import Link from 'next/link'
 import UserTabs from "@/components/layout/UserTabs";
 import {useProfile} from "@/components/layout/UseProfile";
 
-const Users = () => {
+export default function UsersPage() {
+
     const {loading, data} = useProfile();
-    const [redirectToItem, setRedirectToItem] = useState(false);
-
-
-    if (redirectToItem) {
-        return redirect('/users');
-    }
+    const [users, setUsers] = useState([]);
+    
+    useEffect(() => {
+        fetch('/api/users').then(response => {
+            response.json().then(users =>{
+                setUsers(users);
+            });
+        })
+      }, [])
 
     if (loading) {
         return 'Loading User Info ...';
@@ -22,11 +27,25 @@ const Users = () => {
     }
 
   return (
-    <div>
+    <section className='max-w-2xl mx-auto mt-8'>
       <UserTabs isAdmin={true} />
-      users
-    </div>
+      <div className='mt-8'>
+        {users?.length > 0 && (
+            users.map(user => (
+                <div className='bg-gray-100 rounded-lg mb-2 p-1 px-4 flex items-center gap-4' key={user._id}>
+                    <div className='grid grid-cols-2 md:grid-cols-3 gap-4 grow'>
+                       <div className='text-gray-900'> 
+                        {!!user.name && (<span >{user.name }</span>)}
+                        {!user.name && (<span className='italic'>No Name</span>)}
+                      </div>
+                        <span className='text-gray-400'>{user.email }</span>
+                    </div>
+                    <div> 
+                       <Link className='button' href={'/users/'+user._id}>Edit </Link>
+                    </div>
+                </div> 
+            )))}
+      </div>
+    </section>
   )
 }
-
-export default Users
