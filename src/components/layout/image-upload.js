c0b982ebadd49c630d6c90d3e0b4f508834d3a95
ArnoldRamodike <riccoarnold23@gmail.com>
2024-04-16 20:26:24 +0200

    Uploadthing show image

--- src/components/layout/image-upload.js
+++ src/components/layout/image-upload.js
@@ -1,15 +1,18 @@
 'use client'
 
+import Image from 'next/image'
 import { UploadButton } from '../../utils/uploadthing'
 import React from 'react'
 
-const ImageUpload = () => {
+const ImageUpload = ({link, setLink}) => {
   return (
+    <>
     <UploadButton 
       endpoint='imageUploader'
       onClientUploadComplete={(res) => {
         // Do something with the response
         console.log("Files: ", res);
+        setLink(res[0].url);
         alert("Upload Completed");
       }}
       onUploadError={(error) => {
@@ -18,6 +21,15 @@ const ImageUpload = () => {
       }}
       />
 
+      {link && (
+        <Image className="rounded-lg w-full h-full mb-1" src={link} width={250} height={250} alt={'avatar'} />
+      )}
+      {!link && (
+        <div className="text-center bg-gray-200 p-4 text-gray-500 rounded-lg mb-1">
+          No image
+        </div>
+      )}
+</>
   )
 }
 
