4223919d2b02d0208c1fc925b8da0c9dd2c60239
ArnoldRamodike <riccoarnold23@gmail.com>
2024-04-30 17:09:02 +0200

    Fixing the  menu Images

--- src/app/menu-items/page.js
+++ src/app/menu-items/page.js
@@ -40,13 +40,13 @@ const MenuItemsPage = () => {
         </Link>
        </div>
        <div>
-        <h2 className='text-sm text-gray-500 mt-8'>Edid Menu Items</h2>
+        <h2 className='text-sm text-gray-500 mt-8'>Edit Menu Items</h2>
         <div className='grid grid-cols-3 gap-2'>
           {MenuItems?.length > 0 && MenuItems.map(item => (
             <Link href={'menu-items/edit/'+item._id} key={item._id} 
-                className='bg-gray-200 rounded-lg p-4 button '>
-                <div className='relative'>
-                <Image className='rounded-md' src={item.image} alt={'Item Image'} width={100} height={100} />
+                className='bg-gray-200 rounded-lg p-4 button flex flex-col mt-2 '>
+                <div className='h-40 relative overflow-hidden'>
+                <Image className='rounded-md object-cover h-full ' src={item.image} alt={'Item Image'} width={200} height={100} />
                 </div>
                 <div className='text-center'>
                     {item.name}
--- src/components/menu/MenuItemTile.js
+++ src/components/menu/MenuItemTile.js
@@ -1,15 +1,16 @@
+import Image from 'next/image';
 import React from 'react'
 
 export default function MenuItemTile  ({onAddToCart, ...item}){
     const {image, name, description, basePrice, sizes, extraIngridientPrices } = item;
   return (
     <div className="bg-gray-300 p-4 rounded-lg text-center hover:bg-white hover:shadow-md hover:shadow-black/25 transition-all">
-        <div className="text-center">
-            <img className="max-h-auto max-h-24 block mx-auto" src={image} alt={'Meal'}/>
+        <div className="text-center h-40 relative">
+            <Image className="block mx-auto aspect-square object-cover h-full rounded-lg" src={image} alt={'Meal'} width={200} height={100}/>
         </div>
 
         <h4 className="font-semibold my-3 text-xl">{name}</h4>
-        <p className="text-gray-500 text-sm line-clamp-3"> {description}</p>
+        <p className="text-gray-500 text-sm line-clamp-1"> {description}</p>
         <button className="mt-4 bg-primary text-white px-4 py-4" type='button' onClick={onAddToCart}> 
             {(sizes?.length > 0 || extraIngridientPrices?.length > 0) ? (
                 <span>Add to Cart (From ${basePrice}) </span>
